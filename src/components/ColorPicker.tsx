1023aaf8ec6284733ba7070c4dec1b6ed53a7ab8
Illia Pasichnichenko <54638946+ipasic-softserve@users.noreply.github.com>
2025-11-28 19:15:00 +0100

    fix lag when choosing a color with native color picker (#7)

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,25 +1,32 @@
 import { Label } from '@/components/ui/label';
+import { useDebounce } from '@/hooks/useDebounce';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { setSubtitlesColor } from '@/redux/slices/videoConfigSlice';
+import { useEffect, useState } from 'react';
 
 export const ColorPicker = () => {
   const subtitlesColor = useAppSelector((state) => state.videConfig.subtitlesColor);
   const dispatch = useAppDispatch();
+
+  const [color, setColor] = useState(subtitlesColor);
+  const deboucedColor = useDebounce(color, 300);
+
+  useEffect(() => {
+    if (deboucedColor !== subtitlesColor) {
+      dispatch(setSubtitlesColor(deboucedColor));
+    }
+  }, [deboucedColor, subtitlesColor, dispatch]);
 
   return (
     <div className='flex items-center gap-2'>
       <input
         id='subtitles-color'
         type='color'
-        value={subtitlesColor}
-        onChange={(e) => dispatch(setSubtitlesColor(e.target.value))}
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
         className='size-9 cursor-pointer appearance-none rounded-full border-2'
       />
-      <Label
-        htmlFor='subtitles-color'
-        className='cursor-pointer bg-blue-200 p-2'
-        style={{ color: subtitlesColor }}
-      >
+      <Label htmlFor='subtitles-color' className='cursor-pointer bg-blue-200 p-2' style={{ color }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </Label>
     </div>
